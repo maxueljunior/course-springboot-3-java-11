9d928d48104dd27cad3481fc6be76ae12616b295
Maxuel Junior <56198827+maxueljunior@users.noreply.github.com>
2022-07-14 21:46:02 -0300

    PART 2

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.example.course.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.example.course.entities.Pedido;
 import com.example.course.entities.Usuario;
+import com.example.course.repositories.PedidoRepository;
 import com.example.course.repositories.UserRepository;
 
 @Configuration
@@ -16,6 +19,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -23,7 +29,13 @@ public class TestConfig implements CommandLineRunner{
 		Usuario u1 = new Usuario(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
 		Usuario u2 = new Usuario(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		
+		Pedido o1 = new Pedido(null, Instant.parse("2019-06-20T19:53:07Z"), u1); 
+		Pedido o2 = new Pedido(null, Instant.parse("2019-07-21T03:42:10Z"), u2); 
+		Pedido o3 = new Pedido(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
+		
 		userRepository.saveAll(Arrays.asList(u1,u2));
+		pedidoRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
 		
 	}
 	
---
+++ src/main/java/com/example/course/services/PedidoService.java
@@ -0,0 +1,27 @@
+package com.example.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.course.entities.Pedido;
+import com.example.course.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repository;
+	
+	public List<Pedido> findAll(){
+		return repository.findAll();
+	}
+	
+	public Pedido findById(Long id) {
+		Optional<Pedido> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
