6c0692d53dd5a4bb90b7a3c7600a9e6da7fb3049
Maxuel Junior <56198827+maxueljunior@users.noreply.github.com>
2022-07-18 21:32:25 -0300

    User update

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -31,4 +31,17 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public Usuario update(Long id, Usuario obj) {
+		Usuario entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(Usuario entity, Usuario obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+	}
 }
